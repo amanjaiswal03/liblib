6cd2ba7c0e0c80fa491fecc5fa011ad4895ef96b
SwenSchaeferjohann <swen.schaeferjohann@code.berlin>
2019-10-18 19:51:26 +0200

    Nickname form now displays the currentName db

--- imports/ui/profilepage_components/ProfilePage.jsx
+++ imports/ui/profilepage_components/ProfilePage.jsx
@@ -20,30 +20,44 @@ class ProfilePage extends Component {
 
    
     render() { 
-        // console.log(this.props.profiles);
-        return (
-            <div>
-                <form>
-                    <input
-                        type="text"
-                        placeholder= "your name"
-                        onChange={(e)=> {this.setState({username: e.target.value});}}
-                
-                        onBlur={this.handleOnblur.bind(this)}
-                    >
+        
+        
+        let profileList = this.props.profiles;
+            //console.log(this) 
+           // console.log(this.props.Profiles[0]);
+           // console.log(profileList);
+        if (profileList.length>1){
+            let currentUserData = profileList.filter((x) => {return (x._id == Meteor.userId())});
+            // console.log(currentUserData);
+            let currentName = currentUserData[0].profile.nickname;
+
+            return (
+                <div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder= "Add your name"
+                            defaultValue= {currentName}
+                            onChange={(e)=> {this.setState({username: e.target.value});}}
                     
-                    </input>
-            
-
-                </form>
-                <h6>your name will be visible to your friends</h6>
-
-                {/*   User == CurrentUser passed by app.jsx */}
-                <AddBook User = {this.props.User} /> 
-                <MyBookList Profiles={this.props.profiles} User={this.props.User} />
+                            onBlur={this.handleOnblur.bind(this)} //eventually we should make this a button, ux: separate
+                        >
+                        
+                        </input>
                 
-            </div>
-        );
+
+                    </form>
+                    <h6>your name will be visible to your friends</h6>
+
+                    {/*   User == CurrentUser passed by app.jsx */}
+                    <AddBook User = {this.props.User} /> 
+                    <MyBookList Profiles={this.props.profiles} User={this.props.User} />
+                    
+                </div>
+            );
+
+        }
+        return ( <div></div>)
     }
 }
 
